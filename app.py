c2342c9cabe20f3259d7c278b22a5735b953dcb4
hithaishi-cheruku <58849897+hithaishi-cheruku@users.noreply.github.com>
2021-02-02 15:45:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 # cache = Cache(app)
 
 metrics_all = []
+metrics_store = dict()
 @app.route('/')
 def hello_world():
     """
@@ -23,7 +24,6 @@ def hello_world():
 
 
 @app.route('/metrics', methods=['POST'])
-
 def metrics():
     """
     Updates the metrics in system
@@ -33,22 +33,39 @@ def metrics():
         # Check valid payload
         return "Not a valid payload"
     metrics_json = request.get_json()
+    
+    cur_cpu = metrics_json.get('percentage_cpu_used')
+    cur_mem = metrics_json.get('percentage_memory_used')
     metrics_json.update({"ip": remote_add})
+    new_metrics = {'ip': remote_add,'max_cpu': cur_cpu,'max_memory': cur_mem }
 
-    # if cache is needed
-    # cache_all = cache.get('metrics')
-    # if cache_all is None:
-    #     cache.set('metrics', metrics_json)
-    # cache.set('metrics', cache_all)
+    if metrics_store.get(remote_add) is not None:
+
+        old_cpu = metrics_json.get('percentage_cpu_used')
+        old_mem = metrics_json.get('percentage_memory_used')
+
+        new_metrics = {'ip': remote_add, 'max_cpu': max(cur_cpu, old_cpu),'max_memory': max(cur_mem, old_mem)}
+        
+        metrics_store.pop(remote_add)
+        
+    metrics_store.update({remote_add: new_metrics})
     metrics_all.append(metrics_json)
-    return "Updated the system metrics"
+    
+    return "updated"
 
 @app.route('/report')
 def get_report():
     """
     returns report of all records in system
     """
-    return jsonify(metrics_all)
+    remote_add = request.remote_addr
+    if metrics_store.get(remote_add) is None:
+        return "invalid"
+    metrics_list = []
+    for metric in metrics_store.values():
+        metrics_list.append(metric)
+
+    return jsonify(metrics_list)
 
 def main():
     """
